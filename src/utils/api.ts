e615d657bee0d17ca586b4a13629b821a38658b2
JustGimli <aleksnermer@gmail.com>
2023-11-23 15:18:08 +0300

    udpdate

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,7 @@
 
+import axios from "axios"
+
+
 const domain_doctor = 'http://127.0.0.1:48080'
 const domain_patient = 'http://127.0.0.1:50080'
 
@@ -34,4 +37,51 @@ export async function login_patient(login:string, passwd:string){
             console.log(r.json())
         }
     });
+}
+
+
+export const $api = axios.create({
+    baseURL: process.env.REACT_APP_BASE_URL,
+    withCredentials: true,
+})
+
+$api.interceptors.request.use(config => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem('mptok')}`
+    return config
+})
+
+
+
+$api.interceptors.response.use(config => {
+    return config
+}, (async(error) => {
+    const originalRequest = error.config
+
+    if (originalRequest && !originalRequest._isRetry && error.response.status === 401)
+    {
+        originalRequest._isRetry = true
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_BASE_URL}auth/jwt/refresh/`, {refresh: getCookie('mptok')})
+            localStorage.setItem('mptok', response.data.access)
+            return $api.request(originalRequest)
+        }catch(error: any) {
+            if (error.response.status === 401) {}
+                // window.location.href = '/login'
+        }
+    } else {
+        // localStorage.clear()
+        // window.location.href = '/';
+    }
+    
+    throw error
+    
+}))
+
+
+export default function getCookie(name: string) {
+  let matches = document.cookie.match(new RegExp(
+    // eslint-disable-next-line no-useless-escape
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }
