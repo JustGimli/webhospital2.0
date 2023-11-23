93a9557bcf6beca6eb911106b223f5d67d7672c9
JustGimli <aleksnermer@gmail.com>
2023-11-23 10:34:15 +0300

    update

--- src/pages/doctor.tsx
+++ src/pages/doctor.tsx
@@ -1,15 +1,131 @@
-import { AppBar, Button, IconButton, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  Toolbar,
+} from "@mui/material";
 import LogoutIcon from "@mui/icons-material/Logout";
+import Paper from "@mui/material/Paper";
+import { useState } from "react";
 
-export const DoctorPage = () => (
+export const DoctorPage = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleClose = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      {isOpen && <AddPatient open={isOpen} handleClose={handleClose} />}
+      <DoctorAppBar />
+      <div className="flex w-full justify-end">
+        <Button variant="contained" onClick={handleClose}>
+          Добавить пациента
+        </Button>
+      </div>
+      <DockerTable />
+    </>
+  );
+};
+
+const DoctorAppBar = () => (
   <>
+    {" "}
     <AppBar position="static">
       <Toolbar>
         <span style={{ flexGrow: 1 }}>Доктор</span>
         <IconButton color="inherit">
-          <LogoutIcon></LogoutIcon>
+          <LogoutIcon />
         </IconButton>
       </Toolbar>
     </AppBar>
   </>
 );
+
+const DockerTable = () => (
+  <>
+    {" "}
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  </>
+);
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number
+) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
+
+const AddPatient = ({ open, handleClose }: any) => {
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>Subscribe</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="Email Address"
+          type="email"
+          fullWidth
+          variant="standard"
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleClose}>Subscribe</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
