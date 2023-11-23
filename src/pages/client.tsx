78cd245f6f9ea1bbf8208c8eb16ca9786b774612
Lovely Paul <ppetrov228@bk.ru>
2023-11-24 00:17:17 +0300

    hui

--- src/pages/client.tsx
+++ src/pages/client.tsx
@@ -1,3 +1,86 @@
+import {
+  Button,
+  CircularProgress,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import { user } from "..";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AppBarComp } from "../components/appBar";
 
-export const ClientPage = () => <>{user.email}</>;
+export const ClientPage = () => {
+  return (
+    <>
+      <AppBarComp name="Пациент" />
+      <DoctorsTable />
+    </>
+  );
+};
+
+const DoctorsTable = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+      setData(await user.getDoctors());
+      setIsLoading(false);
+    })();
+  }, []);
+
+  return isLoading ? (
+    <div className="flex justify-center w-full">
+      <CircularProgress />
+    </div>
+  ) : data.length === 0 ? (
+    <div className="flex justify-center w-full">
+      Врачи отсутсвуют! Обратитесь в больницу для записи к врачу.
+    </div>
+  ) : (
+    <TableItem data={data} />
+  );
+};
+
+const TableItem = (data: any) => {
+  const handleClick = (number: any) => {
+    navigate("/doctors/card/" + number);
+  };
+
+  const navigate = useNavigate();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>ФИО</TableCell>
+            <TableCell align="right">ФИО</TableCell>
+            <TableCell align="right">Специализация</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row: any) => (
+            <TableRow
+              key={row.doctor_login}
+              style={{ cursor: "pointer" }}
+              onClick={() => handleClick(row.id)} //необходимо обозначить номер врача чтобы переходить к его карточке
+            >
+              <TableCell component="th" scope="row">
+                {row.doctor_login}
+              </TableCell>
+              <TableCell align="right">{row.doctor_login}</TableCell>
+              <TableCell align="right">{row.hospital}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
---
+++ src/pages/doctorCard.tsx
@@ -0,0 +1,170 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Step,
+  StepLabel,
+  Stepper,
+} from "@mui/material";
+import { useState } from "react";
+
+import { AudioRecorder } from "react-audio-voice-recorder";
+
+export const PatientCardPage = () => {
+  const [isOpen, setIsOpen] = useState<any>(false);
+
+  const handleClose = () => setIsOpen(!isOpen);
+
+  return (
+    <>
+      {isOpen && <PatientDialog open={isOpen} handleClose={handleClose} />}
+      <div className="px-5">
+        <span style={{ fontSize: "24px" }}>Информация о враче</span>
+        <ProfileCard />
+        <div className="flex justify-between w-full">
+          <span>Сеансы оценки качества речи</span>
+          <Button variant="contained" onClick={handleClose}>
+            Добавить сеанс
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const ProfileCard = () => {
+  return (
+    <div
+      className="p-10"
+      style={{
+        maxWidth: "300px",
+        border: "1px solid silver",
+        borderRadius: "8px",
+      }}
+    >
+      {data.map((item) => (
+        <>
+          <span>{item.name}: </span>
+          <span>{item.value}</span>
+          <br />
+        </>
+      ))}
+    </div>
+  );
+};
+
+const data = [
+  { name: "ФИО", value: "" },
+  { name: "Специализация", value: "" },
+];
+
+const PatientDialog = ({ open, handleClose }: any) => {
+  const [step, setStep] = useState<any>(0);
+
+  const handleButtonNext = () => {
+    setStep(step + 1);
+    console.log(step);
+    if (step === 2) {
+      setStep(0);
+      handleClose();
+    }
+  };
+
+  return (
+    <>
+      <Dialog open={open} onClose={handleClose} fullWidth>
+        <DialogTitle>
+          <Stepper activeStep={step}>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+        </DialogTitle>
+        <DialogContent>
+          {step === 0 ? (
+            <>
+              <IputType />
+            </>
+          ) : (
+            <>
+              <RecorderVoice />
+            </>
+          )}
+        </DialogContent>
+        <DialogActions>
+          {step === 0 ? (
+            <Button onClick={handleButtonNext}>Вперед</Button>
+          ) : (
+            <Button onClick={handleButtonNext}>Завершить</Button>
+          )}
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+const steps = ["Добавление сеанса", "Загрузка аудио", "Завершение"];
+
+const RecorderVoice = () => {
+  const addAudioElement = (blob: Blob) => {
+    const url = URL.createObjectURL(blob);
+    const audio = document.createElement("audio");
+    audio.src = url;
+    audio.controls = true;
+    document.body.appendChild(audio);
+  };
+
+  return (
+    <>
+      <AudioRecorder
+        onRecordingComplete={addAudioElement}
+        audioTrackConstraints={{
+          noiseSuppression: true,
+          echoCancellation: true,
+          // autoGainControl,
+          // channelCount,
+          // deviceId,
+          // groupId,
+          // sampleRate,
+          // sampleSize,
+        }}
+        onNotAllowedOrFound={(err) => console.table(err)}
+        downloadFileExtension="webm"
+        mediaRecorderOptions={{
+          audioBitsPerSecond: 128000,
+        }}
+        showVisualizer={true}
+      />
+    </>
+  );
+};
+
+const IputType = () => {
+  return (
+    <>
+      <FormControl variant="outlined" fullWidth margin="normal" required>
+        <InputLabel>Тип сеанса</InputLabel>
+        <Select onChange={() => {}} label="Тип сигнала">
+          <MenuItem value="Мужской">Эталонный</MenuItem>
+
+          <MenuItem value="Женский">Не эталонный</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl variant="outlined" fullWidth margin="normal">
+        <InputLabel>Тип сигнала</InputLabel>
+        <Select onChange={() => {}} label="Тип сигнала">
+          <MenuItem value="Мужской">Фразы</MenuItem>
+          <MenuItem value="Женский">Слоги</MenuItem>
+        </Select>
+      </FormControl>
+    </>
+  );
+};
