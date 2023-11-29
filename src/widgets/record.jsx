2d783a82ada47587b751a456297ae0f3973fcae2
JustGimli <aleksnermer@gmail.com>
2023-11-29 04:03:56 +0300

    update

--- src/widgets/record.jsx
+++ src/widgets/record.jsx
@@ -16,6 +16,8 @@ export const RecorderVoiceItem = ({ speechId, handleButtonNext }) => {
   const [isMic, setisMic] = useState(false);
   const [audioData, setAudioData] = useState(null);
 
+  console.log(window.crossOriginIsolated);
+
   useEffect(() => {
     const fetchData = async () => {
       const res = await doctor.getExampleSpeech(patientID, speechId);
@@ -85,13 +87,14 @@ const RecordVoice = ({ speechId, handleIndex, real_val, setAudioData }) => {
       const base64 = reader.result.split(",")[1];
 
       console.log(base64);
+      const wavBase64 = convertToWavBase64(base64);
 
       const data = {
         speech_type:
           speechId.session_type === "фраз"
             ? speechId.session_type + "а"
             : speechId.session_type,
-        base64_value: base64,
+        base64_value: wavBase64,
         base64_value_segment: "",
         real_value: real_val,
       };
@@ -113,11 +116,37 @@ const RecordVoice = ({ speechId, handleIndex, real_val, setAudioData }) => {
             noiseSuppression: true,
             echoCancellation: true,
           }}
-          downloadOnSavePress={true}
-          downloadFileExtension="wav"
+          //   downloadOnSavePress={true}
+          downloadFileExtension="mp3"
           showVisualizer
         />
       </div>
     </div>
   );
+};
+
+const convertToWavBase64 = (base64) => {
+  const binaryData = atob(base64);
+
+  // Convert binary data to WAV format
+  const audioBuffer = new Uint8Array(binaryData.length);
+  for (let i = 0; i < binaryData.length; i++) {
+    audioBuffer[i] = binaryData.charCodeAt(i);
+  }
+
+  const wavBuffer = new Uint8Array(44 + audioBuffer.length);
+  wavBuffer.set(new TextEncoder().encode("RIFF"), 0);
+  wavBuffer.set(new Uint32Array([audioBuffer.length + 36]), 4);
+  wavBuffer.set(new TextEncoder().encode("WAVE"), 8);
+  wavBuffer.set(new TextEncoder().encode("fmt "), 12);
+  wavBuffer.set(new Uint32Array([16, 1, 1, 16000, 32000, 2, 16]), 16);
+  wavBuffer.set(new TextEncoder().encode("data"), 36);
+  wavBuffer.set(new Uint32Array([audioBuffer.length]), 40);
+  wavBuffer.set(audioBuffer, 44);
+
+  // Convert WAV data to base64
+  const resultingData = btoa(String.fromCharCode.apply(null, wavBuffer));
+
+  console.log(resultingData);
+  return resultingData;
 };
