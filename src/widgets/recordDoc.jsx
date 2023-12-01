35d8f883d923a2b63b515fa988f73fdfedde88e0
JustGimli <aleksnermer@gmail.com>
2023-12-01 08:59:33 +0300

    ad

--- src/widgets/recordDoc.jsx
+++ src/widgets/recordDoc.jsx
@@ -123,7 +123,7 @@ const RecordVoice = ({ speechId, handleIndex, real_val, setAudioData }) => {
             echoCancellation: true,
           }}
           downloadOnSavePress={true}
-          downloadFileExtension="mp3"
+          downloadFileExtension="webm"
           showVisualizer
         />
       </div>
