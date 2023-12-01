29fdd6cd4e888ac4790552ee7a4c0f317770469b
JustGimli <aleksnermer@gmail.com>
2023-12-01 06:45:50 +0300

    update

--- src/widgets/phraseChart.tsx
+++ src/widgets/phraseChart.tsx
@@ -1,24 +1,24 @@
-import { BarChart, LineChart } from "@mui/x-charts";
-
+import { LineChart } from "@mui/x-charts";
 import { Dialog, DialogContent } from "@mui/material";
 
 export const PhraseChart = ({ handleCloseChart, open, sessionsData }: any) => {
-  let evals: any[] = [];
-  let x_nums: any[] = [];
-  sessionsData.forEach((element: any) => {
-    if (
+  // Filter sessions with valid data
+  const validSessions = sessionsData.filter(
+    (element: any) =>
       element.session_compares_history.length &&
       element.session_compares_history[0].compared_sessions_id &&
       element.session_compares_history[0].compared_sessions_id.length === 1
-    ) {
-      x_nums.push(element.session_id);
-      evals.push(
-        element.session_compares_history[
-          element.session_compares_history.length - 1
-        ].session_score
-      );
-    }
-  });
+  );
+
+  // Extract data for the chart
+  const x_nums = validSessions.map((element: any) => element.session_id);
+  const evals = validSessions.map(
+    (element: any) =>
+      element.session_compares_history[
+        element.session_compares_history.length - 1
+      ].session_score
+  );
+
   return (
     <Dialog open={open} onClose={handleCloseChart} fullWidth>
       <DialogContent>
@@ -27,11 +27,7 @@ export const PhraseChart = ({ handleCloseChart, open, sessionsData }: any) => {
         ) : (
           <LineChart
             xAxis={[{ data: x_nums }]}
-            series={[
-              {
-                data: evals,
-              },
-            ]}
+            series={[{ data: evals }]}
             width={500}
             height={300}
           />
